80d99cb4e0f96077cb3185cbabda1b2e7b3d655c
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-11-02 19:23:10 +0530

    Update Lab7_q1.cpp

--- Lab7_q1.cpp
+++ Lab7_q1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 class Box {
@@ -14,22 +13,18 @@ int main() {
    Box Box2;        
    double volume = 0.0;
  
-   
    Box1.height = 5.0; 
    Box1.length = 6.0; 
    Box1.breadth = 7.0;
-
    
    Box2.height = 10.0;
    Box2.length = 12.0;
    Box2.breadth = 13.0;
    
-   
    volume = Box1.height * Box1.length * Box1.breadth;
-   cout << "Volume of Box1 : " << volume <<endl;
+   cout << "Volume of Box1 : " << volume <<"\n";
 
-   
    volume = Box2.height * Box2.length * Box2.breadth;
-   cout << "Volume of Box2 : " << volume <<endl;
+   cout << "Volume of Box2 : " << volume <<"\n";
    return 0;
 }
