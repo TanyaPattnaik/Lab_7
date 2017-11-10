d42a1c3fad4d9cb68746e49b727bfaebfe8007f1
TanyaPattnaik <31205790+TanyaPattnaik@users.noreply.github.com>
2017-11-10 20:56:33 +0530

    Update Lab7_q2.cpp

--- Lab7_q2.cpp
+++ Lab7_q2.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
 class Box {
    public:
-      double length;        
-      double breadth;      
-      double height;       
+      double length;         // Length of a box
+      double breadth;        // Breadth of a box
+      double height;         // Height of a box
 
+      // Member functions declaration
       double getVolume(void);
       void setLength( double len );
       void setBreadth( double bre );
       void setHeight( double hei );
 };
 
+// Member functions definitions
 double Box::getVolume(void) {
    return length * breadth * height;
 }
@@ -27,23 +30,28 @@ void Box::setHeight( double hei ) {
    height = hei;
 }
 
+// Main function for the program
 int main() {
-   Box Box1;                
-   Box Box2;                
-   double volume = 0.0;   
+   Box Box1;                // Declare Box1 of type Box
+   Box Box2;                // Declare Box2 of type Box
+   double volume = 0.0;     // Store the volume of a box here
  
+   // box 1 specification
    Box1.setLength(6.0); 
    Box1.setBreadth(7.0); 
    Box1.setHeight(5.0);
 
+   // box 2 specification
    Box2.setLength(12.0); 
    Box2.setBreadth(13.0); 
    Box2.setHeight(10.0);
 
-  volume = Box1.getVolume();
-   cout << "Volume of Box1 : " << volume <<"\n";
+   // volume of box 1
+   volume = Box1.getVolume();
+   cout << "Volume of Box1 : " << volume <<endl;
 
+   // volume of box 2
    volume = Box2.getVolume();
-   cout << "Volume of Box2 : " << volume <<"\n";
+   cout << "Volume of Box2 : " << volume <<endl;
    return 0;
 }
